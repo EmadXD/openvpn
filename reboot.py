9bf04460095b2cd26d87b36bf7e2b7e9afec1c2f
EmadXD <71905699+EmadXD@users.noreply.github.com>
2025-07-13 02:39:19 +0000

    Update reboot.py

--- reboot.py
+++ reboot.py
@@ -9,24 +9,33 @@ tehran_tz = pytz.timezone("Asia/Tehran")
 
 def should_reboot():
     # دریافت زمان فعلی به وقت تهران
-    now = datetime.now(tehran_tz)
-    # بررسی بازه زمانی بین 4 صبح تا 6 صبح
-    if now.hour >= 4 and now.hour < 5:
-        return False
-    return True
+    try:
+        now = datetime.now(tehran_tz)
+        # بررسی بازه زمانی بین 4 صبح تا 6 صبح
+        if now.hour >= 4 and now.hour < 5:
+            return False
+        return True
+    except:
+        return True
 
 
 def reboot_server():
     # اجرای دستور ریبوت
-    os.system("sudo reboot")
+    try:
+        os.system("sudo reboot")
+    except:
+        print("-")
 
 
 if __name__ == "__main__":
     while True:
-        time.sleep(3600)
-        if should_reboot():
-            print("Rebooting the server...")
-            reboot_server()
-        else:
-            print("Skipping reboot during restricted hours (4 AM to 6 AM Tehran time).")
+        try:
+            time.sleep(3600)
+            if should_reboot():
+                print("Rebooting the server...")
+                reboot_server()
+            else:
+                print("Skipping reboot during restricted hours (4 AM to 6 AM Tehran time).")
+        except:
+            print("--")
         # صبر کردن برای یک ساعت (3600 ثانیه)
